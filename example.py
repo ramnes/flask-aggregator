2d502ce8a6bb3e692a62cbe34776c9b813ae631c
ramnes <contact@ramnes.eu>
2017-01-09 01:15:23 +0100

    Add an example application to play with

---
+++ example.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from flask_aggregator import Aggregator
+
+app = Flask(__name__)
+Aggregator(app, "/batch")
+
+
+@app.route("/hello/<name>", methods=["GET"])
+def hello_name(name):
+    response = "Hello, {}!".format(name)
+    question = request.args.get("question")
+    if question:
+        response += " " + question
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
